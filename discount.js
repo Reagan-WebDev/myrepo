24620742cab0d447c343c884ec5fd6c8d49b4d07
Reagan <oluochreagano@gmail.com>
2024-11-24 16:57:18 -0800

    Discount

---
+++ discount.js
@@ -0,0 +1,15 @@
+const myInput = document.getElementById("myInput");
+const myButton = document.getElementById("myButton");
+const myP = document.getElementById("myP");
+let PriceOfCommodity;
+document.getElementById("myButton").onclick = function() {
+    PriceOfCommodity = document.getElementById("myInput").value;
+    PriceOfCommodity = Number(PriceOfCommodity);
+    if(PriceOfCommodity > 1500){
+        PriceOfCommodity = PriceOfCommodity - (PriceOfCommodity * (10 / 100));
+        return myP.textContent = "Ksh" + PriceOfCommodity;
+    }
+    else{
+        return myP.textContent = "Ksh" + PriceOfCommodity;
+    }
+}
